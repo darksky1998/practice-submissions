1a13f910733174f2906384b1c1fe0989c0420307
darksky1998 <126346718+darksky1998@users.noreply.github.com>
2026-06-09 21:50:06 +0530

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    List<List<String>> res = new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        dfs(s,0,new ArrayList<>());
+        return res;
+    }
+    void dfs(String s, int i,List<String> cur){
+        if(i>=s.length()){
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+        for(int j = i;j<s.length();j++){
+            if(isPallin(s,i,j)){
+                cur.add(s.substring(i,j+1));
+                dfs(s,j+1,cur);
+                cur.remove(cur.size()-1);
+            }
+        }
+    }
+
+    boolean isPallin(String s, int l, int r){
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
